381cf58d35ce6435fdccfc8a9a4fb409bba7d133
haobo23 <haobo10089@gmail.com>
2023-11-06 16:50:49 -0800

    wip: add skeleton code for react frontend with router

---
+++ frontend/src/pages/Page3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page3 = () => {
+  return <div>This is Page 3</div>;
+};
+
+export default Page3;
---
+++ frontend/src/pages/Page4.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page4 = () => {
+  return <div>This is Page 4</div>;
+};
+
+export default Page4;
