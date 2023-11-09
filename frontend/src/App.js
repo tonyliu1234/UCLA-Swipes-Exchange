08745890e9963233cdfbaa2d114238c0de915831
tonyliu1234 <iamtony200110@gmail.com>
2023-11-08 16:11:29 -0800

    created user profile, change user profile, sign in and sign up pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Page1 from './pages/Page1';
-import Page2 from './pages/Page2';
-import Page3 from './pages/Page3';
-import Page4 from './pages/Page4';
-import Page5 from './pages/Page5';
-import Page6 from './pages/Page6';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
+import Page5 from "./pages/Page5";
+import Page6 from "./pages/Page6";
+import userProfile from "./pages/userProfile";
+import signIn from "./pages/signIn";
+import signUp from "./pages/signUp";
+import changeUserProfile from "./pages/changeUserProfile";
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
         <Route path="/page4" component={Page4} />
         <Route path="/page5" component={Page5} />
         <Route path="/page6" component={Page6} />
+        <Route path="/userProfile" component={userProfile} />
+        <Route path="/signIn" component={signIn} />
+        <Route path="/signUp" component={signUp} />
+        <Route path="/changeUserProfile" component={changeUserProfile} />
       </Switch>
     </Router>
   );
