ad80d9312e66ab1b86963f4823eaccda4f473a21
Mark Li <limarksmail@gmail.com>
2023-11-08 19:37:50 -0800

    [Issue #14] Registration with Email and Password (#21)
    * Registration with email and password
    
    * bug fix
    
    * fix user creation process
    
    * Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from dbconnection import DBConnection, DBOperations
+# from flask_login import LoginManager, login_required  # noqa: F401
+from user import User
+import hashlib
 
 db_connection = DBConnection()
 db_connection.connect()
@@ -12,14 +15,34 @@ order_operations = DBOperations(db_connection, 'orders')
 
 app = Flask(__name__)
 
-
 @app.route('/register', methods=['POST'])
 def register():
-  pass
+    data = request.get_json()
+
+    name = data.get('name')
+    email = data.get('email')
+    phone = data.get('phone')
+    password = data.get('password')
+
+    if user_operations.get(email):
+        return jsonify({'status': 'error', 'message': 'Email already in use'}), 409
+    
+    def hash_password(password):
+      password_bytes = password.encode('utf-8')
+      hash_object = hashlib.sha256(password_bytes)
+      return hash_object.hexdigest()
+
+    hashed_password = hash_password(password)
+    user = User(name, email, phone, hashed_password)
+    try:
+      user_id = user_operations.create(user.binary_value)
+      return jsonify({'status': 'success', 'message': 'User {0} registered successfully'.format(user_id)}), 201
+    except:
+      return jsonify({'status': 'error', 'message': 'Registration failed'}), 500
 
 @app.route('/login', methods=['POST'])
 def login():
-  pass
+    pass
 
 @app.route('/orders', methods=['GET'])
 def get_orders():
