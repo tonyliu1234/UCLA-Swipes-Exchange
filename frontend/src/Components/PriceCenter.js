78cc9b4853e980109bfa57136e22464beaedf652
Jeffrey Yu <35394596+JeffreytheCoder@users.noreply.github.com>
2023-11-09 12:39:48 -0800

    Home page with action bar & price center (#37)
    * Create home page with ActionCard & PriceCenter
    
    * Add home to routes & remove native pages

---
+++ frontend/src/Components/PriceCenter.js
@@ -0,0 +1,41 @@
+import { Box, Button, Typography } from '@mui/material';
+import React from 'react';
+
+const PriceCenter = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="subtitle1">Last Sale: $8.7</Typography>
+        <Box
+          sx={{
+            color: 'red',
+            display: 'flex',
+            alignItems: 'center',
+            gap: 0.5,
+          }}
+        >
+          <Typography variant="subtitle1">↓</Typography>
+          <Typography variant="subtitle1">-$1.8 (-12%)</Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Button variant="text">View Asks</Button>
+        <Button variant="text">View Bids</Button>
+        <Button variant="text">View Sales</Button>
+      </Box>
+    </>
+  );
+};
+
+export default PriceCenter;
