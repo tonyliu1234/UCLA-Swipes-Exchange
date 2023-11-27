f1e879126f460a2d7f35dc19537d797be4db6d1f
Simon Yuan <simonyuan@SimonMBP-2023.lan>
2023-11-26 19:07:48 -0800

    feat: add controller code for ask order creation logic

--- frontend/src/pages/AskOrder.js
+++ frontend/src/pages/AskOrder.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography, Box, MenuItem, FormControl, Select, InputLabel, Input, ToggleButtonGroup, ToggleButton, Button } from "@mui/material";
 import styled from '@emotion/styled';
 
@@ -34,78 +34,6 @@ const ToggleContainer = styled.div`
   marginTop: 1.2Rem;
 `;
 
-// const useStyles = makeStyles({
-//   root: {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     height: '100vh', // Full height of the viewport
-//     width: '100vw', // Full width of the viewport
-//     backgroundImage: `url(https://source.unsplash.com/random?wallpapers)`,
-//     backgroundSize: 'cover', // Cover the entire viewport
-//     backgroundPosition: 'center', // Center the background image
-//   },
-//   container: {
-//     width: '50%', // Half the width of the screen
-//     maxWidth: '600px', // Optional: if you want to limit how wide it can get
-//     margin: '0', // No margin for centering purposes
-//     padding: '16px',
-//     boxShadow: '0px 3px 6px #00000029',
-//     borderRadius: '4px',
-//     backgroundColor: 'rgba(255, 255, 255, 0.5)', // Adjust transparency here
-//     backdropFilter: 'blur(8px)', // Apply a blur effect to the background content
-//   },
-//   // Add this inside your useStyles
-//   toggleContainer: {
-//     display: 'flex',
-//     justifyContent: 'center', // This will center the toggle buttons wrapper div
-//     alignItems: 'center',
-//     margin: '0 auto', // Centers the container itself if needed
-//     width: '100%', // Ensures the container takes the full width
-//     marginBottom: '2Rem',
-//     marginTop: '1.2Rem',
-//   },
-
-//   input: {
-//     flex: 1,
-//     margin: '8px',
-//   },
-// });
-
-// const StyledToggleButtonGroup = withStyles({
-//   grouped: {
-//     margin: '8px',
-//     border: 'none',
-//     '&:not(:first-child)': {
-//       borderRadius: '20px',
-//     },
-//     '&:first-child': {
-//       borderRadius: '20px',
-//     },
-//     // Flexbox properties to center the buttons
-//     display: 'flex', 
-//     justifyContent: 'center', // This centers the buttons horizontally in the container
-//     height: '2.4Rem',
-//     width: '18Rem'
-//   },
-// })(ToggleButtonGroup);
-
-// const StyledToggleButton = withStyles({
-//   root: {
-//     border: '1px solid #c4c4c4',
-//     '&.Mui-selected': {
-//       backgroundColor: '#000',
-//       color: '#fff',
-//       '&:hover': {
-//         backgroundColor: '#000',
-//       },
-//     },
-//     // Text alignment properties to center text inside the button
-//     textAlign: 'center', 
-//     lineHeight: '40px', // Adjust the line height to match your button's height
-//   },
-// })(ToggleButton);
-
 const StyledToggleButtonGroup = styled(ToggleButtonGroup)`
   .MuiToggleButtonGroup-grouped {
     margin: 8px;
@@ -136,9 +64,74 @@ const StyledToggleButton = styled(ToggleButton)`
   line-height: 40px;
 `;
 
+const fetchOrderStats = async () => {
+  try {
+    const response = await fetch('http://localhost:5000');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data; // Assuming data contains fields like { totalAsks: number, largestBid: number }
+  } catch (error) {
+    console.error('Error fetching order stats:', error);
+  }
+}
+
+const submitAskOrder = async (price, size) => {
+  const order = {
+    price: price,
+    size: size, // Size is assumed to be a part of the order, based on your frontend
+    // Other fields as required by your backend
+  };
+
+  try {
+    const response = await fetch('http://localhost:5000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(order),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error submitting ask order:', error);
+  }
+}
+
+
+
 
 
 const AskOrder = () => {
+  useEffect(() => {
+    fetchOrderStats().then(stats => {
+      if (stats) {
+        // Update your state or UI with the fetched stats
+        console.log('Total asks:', stats.totalAsks);
+        console.log('Largest bid:', stats.largestBid);
+      }
+    });
+  }, []);
+
+  const [price, setPrice] = useState('');
+  const [size, setSize] = useState('');
+
+
+  const handleSubmit = () => {
+    // Assuming you have state variables for price and size
+    submitAskOrder(price, size).then(response => {
+      // Handle response, maybe clear the form or show a success message
+      console.log('Order submitted:', response);
+    });
+    setPrice('');
+    setSize('');
+  }
+  
   const [alignment, setAlignment] = useState('left');
 
   const handleAlignment = (event, newAlignment) => {
@@ -149,7 +142,7 @@ const AskOrder = () => {
     <Root>
     <Container>
       <Typography variant="subtitle1" gutterBottom>
-        Inventory is High, Act Fast - There are 16 asks on this item.
+        Inventory is High, Act Fast - There are {price} asks on this item.
       </Typography>
       <Typography variant="h6">Size: US XL</Typography>
       <ToggleContainer> {/* Wrap your StyledToggleButtonGroup with a div */}
@@ -185,7 +178,7 @@ const AskOrder = () => {
       </FormControl>
       <Box display="flex" justifyContent="space-between" my={2}>
         <Button variant="text" style={{color: 'black'}}>Cancel</Button>
-        <Button variant="contained" color="grey">
+        <Button variant="contained" color="grey" onClick={handleSubmit}>
           Submit
         </Button>
       </Box>
