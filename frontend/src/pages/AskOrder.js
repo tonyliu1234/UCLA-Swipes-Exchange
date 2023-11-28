844742f993ed8ae08aec86b6a9ec2e3936abf612
Simon Yuan <simonyuan@SimonMBP-2023.lan>
2023-11-27 17:40:24 -0800

    fix: fix controller code for creating ask order

--- frontend/src/pages/AskOrder.js
+++ frontend/src/pages/AskOrder.js
@@ -66,16 +66,37 @@ const StyledToggleButton = styled(ToggleButton)`
 
 const fetchOrderStats = async () => {
   try {
-    const response = await fetch('http://localhost:5000');
+    const response = await fetch('http://localhost:5000/orders', { // Adjust the URL to where your orders are fetched from
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json();
-    return data; // Assuming data contains fields like { totalAsks: number, largestBid: number }
+    const orders = await response.json();
+    
+    // Process orders to calculate stats
+    let askCount = 0;
+    let largestBidPrice = 0;
+
+    orders.forEach(order => {
+      if (order.side === 'ASK') {
+        askCount++;
+      }
+      if (order.side === 'BID' && order.price > largestBidPrice) {
+        largestBidPrice = order.price;
+      }
+    });
+
+    return { totalAsks: askCount, largestBid: largestBidPrice || 0 };
   } catch (error) {
     console.error('Error fetching order stats:', error);
   }
-}
+};
+
 
 const submitAskOrder = async (price, size) => {
   const order = {
@@ -108,19 +129,31 @@ const submitAskOrder = async (price, size) => {
 
 
 const AskOrder = () => {
+  const [price, setPrice] = useState(10);
+  const [size, setSize] = useState('');
+
+  const [totalAsks, setTotalAsks] = useState(0);
+  const [largestBid, setLargestBid] = useState(0);
+  const [alignment, setAlignment] = useState('left');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
   useEffect(() => {
     fetchOrderStats().then(stats => {
       if (stats) {
-        // Update your state or UI with the fetched stats
-        console.log('Total asks:', stats.totalAsks);
-        console.log('Largest bid:', stats.largestBid);
+        setTotalAsks(stats.totalAsks);
+        setLargestBid(stats.largestBid);
+        // Now you have totalAsks and largestBid to use in your UI
+      }
+      if (alignment === 'right') { // 'right' corresponds to 'Sell Now'
+        setPrice(20); // Set price to the largest bid
+      } else {
+        setPrice(0);
       }
     });
-  }, []);
-
-  const [price, setPrice] = useState('');
-  const [size, setSize] = useState('');
-
+  }, [alignment, largestBid]);
 
   const handleSubmit = () => {
     // Assuming you have state variables for price and size
@@ -132,17 +165,12 @@ const AskOrder = () => {
     setSize('');
   }
   
-  const [alignment, setAlignment] = useState('left');
-
-  const handleAlignment = (event, newAlignment) => {
-    setAlignment(newAlignment);
-  };
 
   return (
     <Root>
     <Container>
       <Typography variant="subtitle1" gutterBottom>
-        Inventory is High, Act Fast - There are {price} asks on this item.
+        Inventory is High, Act Fast - There are {totalAsks} asks on this item.
       </Typography>
       <Typography variant="h6">Size: US XL</Typography>
       <ToggleContainer> {/* Wrap your StyledToggleButtonGroup with a div */}
@@ -163,7 +191,7 @@ const AskOrder = () => {
     </ToggleContainer>
       <FormControl fullWidth>
         <InputLabel htmlFor="price">Price</InputLabel>
-        <Input id="price" style={{marginBottom: '2Rem'}} startAdornment={<Typography variant="h6">$</Typography>} />
+        <Input id="price" style={{marginBottom: '2Rem'}} startAdornment={<Typography variant="h6">${price}</Typography>} />
       </FormControl>
       <FormControl fullWidth>
         <InputLabel id="ask-expiration-label">Ask Expiration</InputLabel>
