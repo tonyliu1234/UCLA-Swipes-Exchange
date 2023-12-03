24e39a763e6afeac601cb28d800788bfe3813092
xhsuj <87062537+xhsuj@users.noreply.github.com>
2023-12-02 22:26:32 -0800

    feature: add pages for accessing user's own asks oreders bid orders (#70)
    Co-authored-by: Simon Yuan <simonyuan@SimonMBP-2023.lan>

---
+++ frontend/src/pages/AsksPage.js
@@ -0,0 +1,55 @@
+// BidsPage.js
+import React, { useState, useEffect } from 'react';
+import { List, ListItem, ListItemText, Card, Typography, Button } from '@mui/material';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import axios from 'axios'; // Assuming you are using axios for API calls
+
+const AsksPage = () => {
+    const [asks, setAsks] = useState([]);
+    const history = useHistory();
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const response = await axios.get('/order/list_order'); // Adjust this API endpoint as needed
+                const asks = response.data.filter(order => order.side === 'ASK');
+                setAsks(asks);
+            } catch (error) {
+                console.error('Error fetching orders:', error);
+            }
+        };
+
+        fetchOrders();
+    }, []);
+    const goBack = () => {
+        history.push('userProfile');
+    };
+    return (
+        <>
+            <Button onClick={goBack} style={{ margin: '20px' }}>Back to Profile</Button>
+        <List>
+            {asks.map(ask => (
+                <ListItem key={ask._id}>
+                    <Card variant="outlined" style={{ width: '100%' }}>
+                        <ListItemText 
+                            primary={`$${ask.price}`}
+                            secondary={
+                                <>
+                                  <Typography component="span">
+                                    {`Posted: ${new Date(ask.posted).toLocaleString()}`}
+                                  </Typography>
+                                  <Typography component="span" style={{ display: 'block' }}>
+                                  {`Matching Status: ${ask.is_matched ? "matched" : "not matched"}`}
+                                  </Typography>
+                                </>
+                              }
+                        />
+                    </Card>
+                </ListItem>
+            ))}
+        </List>
+        </>
+        
+    );
+};
+
+export default AsksPage;
