909cdd1df8c115041d5cd8fd0ac949e2cc1d8407
tonyliu1234 <96612618+tonyliu1234@users.noreply.github.com>
2023-12-02 22:50:00 -0800

    feature: integrated notification and connected askspage and bidspage (#71)
    * implemented notification page
    
    * connected askpage and bidspage to user profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import userProfile from './pages/userProfile';
-import signIn from './pages/signIn';
-import signUp from './pages/signUp';
-import changeUserProfile from './pages/changeUserProfile';
-import AskOrder from './pages/AskOrder';
-import BidOrder from './pages/BidOrder';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import userProfile from "./pages/userProfile";
+import signIn from "./pages/signIn";
+import signUp from "./pages/signUp";
+import changeUserProfile from "./pages/changeUserProfile";
+import AskOrder from "./pages/AskOrder";
+import BidOrder from "./pages/BidOrder";
+import Home from "./pages/Home";
+import Notification from "./pages/notification";
+import AsksPage from "./pages/AsksPage";
+import BidsPage from "./pages/BidsPage";
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
         <Route path="/ask" component={AskOrder} />
         <Route path="/bid" component={BidOrder} />
         <Route exact path="/" component={Home} />
+        <Route path="/notification" component={Notification} />
+        <Route path="/AsksPage" component={AsksPage} />
+        <Route path="/BidsPage" component={BidsPage} />
       </Switch>
     </Router>
   );
