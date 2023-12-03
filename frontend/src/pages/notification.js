d2108dc182676f0ff233cee661308a4669b3151a
tonyliu1234 <iamtony200110@gmail.com>
2023-12-02 23:03:41 -0800

    updated notification page to accomodate changes in api

--- frontend/src/pages/notification.js
+++ frontend/src/pages/notification.js
@@ -5,23 +5,48 @@ import {
   ListItemText,
   Divider,
   Typography,
+  ListItemSecondaryAction,
+  IconButton,
 } from "@mui/material";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
 import axios from "axios";
 
 function NotificationsPage() {
   const [notifications, setNotifications] = useState([]);
+  const useMockData = true; // Set to false to fetch data from the API
 
   useEffect(() => {
-    // Fetch notifications
-    axios
-      .get("/notifications")
-      .then((response) => {
-        setNotifications(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching notifications:", error);
-      });
-  }, []);
+    if (useMockData) {
+      // Mock data
+      const mockNotifications = [
+        {
+          side: "BUY",
+          client: "Alice Johnson",
+          client_phone: "123-456-7890",
+          client_email: "alice@example.com",
+        },
+        {
+          side: "SELL",
+          client: "Bob Smith",
+          client_phone: "987-654-3210",
+          client_email: "bob@example.com",
+        },
+        // ... more mock notifications
+      ];
+      setNotifications(mockNotifications);
+    } else {
+      // Fetch notifications from the API
+      axios
+        .get("/user/register")
+        .then((response) => {
+          setNotifications(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching notifications:", error);
+        });
+    }
+  }, [useMockData]);
 
   return (
     <div style={{ margin: "20px" }}>
@@ -38,9 +63,17 @@ function NotificationsPage() {
             <React.Fragment key={index}>
               <ListItem>
                 <ListItemText
-                  primary={notification.title}
-                  secondary={notification.message}
+                  primary={`${notification.side} - ${notification.client}`}
+                  secondary={`Phone: ${notification.client_phone} | Email: ${notification.client_email}`}
                 />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="phone">
+                    <PhoneIcon />
+                  </IconButton>
+                  <IconButton edge="end" aria-label="email">
+                    <EmailIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
               </ListItem>
               {index < notifications.length - 1 && <Divider />}
             </React.Fragment>
