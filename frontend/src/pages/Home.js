8912704309152378adb43ae1ba1ca8ddbba697e7
Jeffrey Yu <35394596+JeffreytheCoder@users.noreply.github.com>
2023-12-08 15:21:13 -0800

    remove comment

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,70 +5,13 @@ import PriceCenter from '../Components/PriceCenter';
 import { grey } from '@mui/material/colors';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
-// const mockData = [
-//   {
-//     _id: '6567264bd623ad3e4a6a0cfd',
-//     is_matched: false,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Wed, 29 Nov 2023 03:53:47 GMT',
-//     price: 9,
-//     side: 'BID',
-//   },
-//   {
-//     _id: '65672aa16d0ea2f52bfd7517',
-//     is_matched: false,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Wed, 29 Nov 2023 04:12:17 GMT',
-//     price: 8,
-//     side: 'ASK',
-//   },
-//   {
-//     _id: '65672ac0d11d51e8ec3c7cee',
-//     is_matched: true,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Mon, 27 Nov 2023 04:12:48 GMT',
-//     price: 9,
-//     side: 'BID',
-//   },
-//   {
-//     _id: '65672ad37be56623d5913458',
-//     is_matched: true,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Tue, 26 Nov 2023 04:13:07 GMT',
-//     price: 10,
-//     side: 'ASK',
-//   },
-//   {
-//     _id: '6567fe3a68e54896946c619b',
-//     is_matched: false,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Wed, 29 Nov 2023 19:15:06 GMT',
-//     price: 8,
-//     side: 'BID',
-//   },
-//   {
-//     _id: '6567ff3079af9914d23f7e65',
-//     is_matched: false,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Wed, 29 Nov 2023 19:19:12 GMT',
-//     price: 7,
-//     side: 'ASK',
-//   },
-//   {
-//     _id: '6567ff4682ab59430b3bb34a',
-//     is_matched: true,
-//     owner_id: '65671a2fada3e0383290e582',
-//     posted: 'Wed, 29 Nov 2023 19:19:34 GMT',
-//     price: 9,
-//     side: 'BID',
-//   },
-// ];
-
-const apiUrl = process.env.REACT_APP_API_URL === undefined ? "" : process.env.REACT_APP_API_URL
+const apiUrl =
+  process.env.REACT_APP_API_URL === undefined
+    ? ''
+    : process.env.REACT_APP_API_URL;
 
 const fetchOrders = async () => {
   try {
-    // TODO: change to prod endpoint
     const response = await fetch(`${apiUrl}/order/list_all_order`, {
       method: 'GET',
       headers: {
@@ -91,11 +34,12 @@ const Home = () => {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    fetchOrders().then(data => {
-    if (data) {
-      setOrders(data);
-      // Now you have totalAsks and largestBid to use in your UI
-    }})
+    fetchOrders().then((data) => {
+      if (data) {
+        setOrders(data);
+        // Now you have totalAsks and largestBid to use in your UI
+      }
+    });
   }, []);
 
   return (
